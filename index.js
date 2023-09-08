75273476342ea961a1df5689478b490de88ec501
aashfaque <v-aashfaque@egencia.com>
2023-09-08 19:46:24 +0400

    Animation,Styles,JS etc

--- index.js
+++ index.js
@@ -25,4 +25,18 @@ window.onscroll = () => {
 
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
+
+//toggle navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
